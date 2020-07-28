13067361820cd4afd63b3aad99f4edb5ea0bba12
Chetan Badhe <csbadhe08@gmail.com>
2020-07-28 17:42:36 +0530

    ft: add sendinblue api

---
+++ api/joinWaitlist.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+module.exports = (req, res) => {
+  const email = req.body.email;
+
+  const headers = {
+    accept: 'application/json',
+    'content-type': 'application/json',
+    'api-key': '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'
+  };
+
+  const requestConfig = {
+    headers: headers,
+  };
+
+  const body = {
+    updateEnabled: false,
+    email: email
+  };
+
+  const data = JSON.stringify(body);
+
+  axios.post('https://api.sendinblue.com/v3/contacts', data, requestConfig)
+    .then(response => {
+      res.json({
+          status: 'success'
+        })
+    })
+    .catch(err => {
+      res.json(err)
+    })
+
+}
